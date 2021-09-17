b4b18524e9e34384b6b67d83669876c2993d9fc4
Alejandro Cardozo <uri.cardozo@gmail.com>
2021-09-17 16:08:47 -0300

    restructuring the ui

--- src/components/Translate.jsx
+++ src/components/Translate.jsx
@@ -1,37 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import classes from './Translate.module.css';
-import { Form, TextArea, Button, Icon } from 'semantic-ui-react';
+import { Form, TextArea, Button, Icon, Grid } from 'semantic-ui-react';
 import axios from 'axios';
 
 export default function Translate() {
   const [inputText, setInputText] = useState('');
-  const [detectLanguageKey, setDetectedLanguageKey] = useState('');
+  const [sourceLanguage, setSourceLanguage] = useState('');
+  const [targetLanguage, setTargetLanguage] = useState('');
   const [languagesList, setLanguagesList] = useState([]);
-  const [selectedLanguageKey, setLanguageKey] = useState('');
   const [resultText, setResultText] = useState('');
 
-  const getLanguageSource = () => {
-    axios
-      .post(`https://libretranslate.de/detect`, {
-        q: inputText,
-      })
-      .then((response) => {
-        setDetectedLanguageKey(response.data[0].language);
-      })
-      .catch((e) => console.log(e));
+  const sourceLanguageKeyHandler = (selectedLanguage) => {
+    setSourceLanguage(selectedLanguage.target.value);
   };
 
-  const languageKey = (selectedLanguage) => {
-    setLanguageKey(selectedLanguage.target.value);
+  const targetLanguageKeyHandler = (selectedLanguage) => {
+    setTargetLanguage(selectedLanguage.target.value);
   };
 
   const translateText = () => {
-    getLanguageSource();
-
     let data = {
       q: inputText,
-      source: detectLanguageKey,
-      target: selectedLanguageKey,
+      source: sourceLanguage,
+      target: targetLanguage,
     };
     axios
       .post(`https://libretranslate.de/translate`, data)
@@ -42,55 +33,72 @@ export default function Translate() {
   };
 
   useEffect(() => {
-    axios.get(`https://libretranslate.de/languages`)
-    .then((response) => {
-     setLanguagesList(response.data)
-    })
-
-    getLanguageSource()
- }, [inputText])
+    axios.get(`https://libretranslate.de/languages`).then((response) => {
+      setLanguagesList(response.data);
+    }).catch(e => console.log(e));
+  }, []);
 
   return (
     <>
       <div className={classes['app-header']}>
         <h2 className={classes['header']}>React Translator</h2>
+        <Form>
+          <Grid>
+            <Grid.Row columns={2}>
+              <Grid.Column>
+                <select
+                  className={classes['language-select']}
+                  onChange={sourceLanguageKeyHandler}
+                >
+                  <option>Please select your language..</option>
+                  {languagesList.map((language) => {
+                    return (
+                      <option key={language.code} value={language.code}>
+                        {language.name}
+                      </option>
+                    );
+                  })}
+                </select>
+                <Form.Field
+                  control={TextArea}
+                  placeholder='Type Text to Translate..'
+                  onChange={(e) => setInputText(e.target.value)}
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <select
+                  className={classes['language-select']}
+                  onChange={targetLanguageKeyHandler}
+                >
+                  <option>Please select your target language..</option>
+                  {languagesList.map((language) => {
+                    return (
+                      <option key={language.code} value={language.code}>
+                        {language.name}
+                      </option>
+                    );
+                  })}
+                </select>
+                <Form.Field
+                  control={TextArea}
+                  placeholder='Your Result Translation..'
+                  value={resultText}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
 
-        <div className={classes['app-body']}>
-          <div>
-            <Form>
-              <Form.Field
-                control={TextArea}
-                placeholder='Type Text to Translate..'
-                onChange={(e) => setInputText(e.target.value)}
-              />
-
-              <select
-                className={classes['language-select']}
-                onChange={languageKey}
-              >
-                <option>Please Select Language..</option>
-                {languagesList.map((language) => {
-                  return (
-                    <option key={language.code} value={language.code}>
-                      {language.name}
-                    </option>
-                  );
-                })}
-              </select>
-
-              <Form.Field
-                control={TextArea}
-                placeholder='Your Result Translation..'
-                value={resultText}
-              />
-
-              <Button color='orange' size='large' onClick={translateText}>
-                <Icon name='translate' />
-                Translate
-              </Button>
-            </Form>
-          </div>
-        </div>
+          <Button
+            style={{ margin: '15px' }}
+            color='orange'
+            size='large'
+            disabled={!sourceLanguage || !targetLanguage}
+            onClick={translateText}
+          >
+            <Icon name='translate' />
+            Translate
+          </Button>
+        </Form>
       </div>
     </>
   );
